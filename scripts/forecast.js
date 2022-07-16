18c8fa1cb6b10a19976518a22f000582151c8ffc
Shaharyar Ali Mujeeb <60549948+Sherry-tech@users.noreply.github.com>
2022-07-16 17:33:17 +0500

    WeatherApi

---
+++ scripts/forecast.js
@@ -0,0 +1,31 @@
+const key = '2849uvgrlqvNWovwhTevBjeXpGGxxKJT';
+
+
+const getCity = async (city) => {
+    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+    const query = `?apikey=${key}&q=${city}`;
+
+    const response = await fetch(base + query);
+    const data = await response.json();
+
+    return data[0];
+};
+
+const getWeather = async (id) => {
+    const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+    const query = `${id}?apikey=${key}`;
+
+    const response = await fetch(base + query);
+    const data = await response.json();
+
+    return data[0];
+};
+
+// getCity('multan')
+//     .then((data)=>{
+//         return getWeather(data.Key);
+//     }).then((data)=>{
+//         console.log(data)
+//     })
+//     .catch(err => console.log(err));
+// //weather(260624);
